7903127688d7be2935d699b0f482c8bbb5a1f3a3
gnazarkin <gleb.nazarkin@gmail.com>
2015-05-12 15:58:51 -0400

    Implemented basic testing

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -1,19 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'tr',
-	article: null, //passed-in
-	articleStates: null, // passed-in
-	autoSave: function() {
-		var article = this.get('article');
-		if (!article.get('isNew')) {
-			this.sendAction('save', article);
-		}
-	},
-	stateChanged: Ember.on('init', Ember.observer('article.state', function() {
-		var article = this.get('article');
-		if (article.get('isDirty') && !article.get('isSaving')) {
-			Ember.run.once(this, this.autoSave);
-		}
-	}))
+  tagName: 'tr',
+  article: null, // passed-in
+  articleStates: null, // passed-in
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
+      this.sendAction('save', article);
+    }
+  },
+  stateChanged: function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  }.on('init').observes('article.state')
 });
